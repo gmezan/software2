e8b6d0c660737d72ad9081f6ed5eae262c344603
Alex Montes <montes.alex@pucp.edu.pe>
2020-05-04 15:32:46 -0500

    tablassSSSS

--- src/main/resources/static/js/demo/datatables-demo.js
+++ src/main/resources/static/js/demo/datatables-demo.js
@@ -10,3 +10,20 @@ $(document).ready(function() {
     "order": [[ 0, 'desc' ]]
   });
 });
+
+/*
+$(document).ready(function() {
+  $('a[data-toggle="tab"]').on( 'shown.bs.tab', function (e) {
+    $.fn.dataTable.tables( {visible: true, api: true} ).columns.adjust();
+  } );
+
+  $('table.table').DataTable( {
+    ajax:           '../ajax/data/arrays.txt',
+    scrollY:        200,
+    scrollCollapse: true,
+    paging:         false
+  } );
+
+  // Apply a search to the second table for the demo
+  $('#myTable2').DataTable().search( 'New York' ).draw();
+} );
