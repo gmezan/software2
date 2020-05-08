dff307d4e7a757d6712bf2b1887dd7a3a8945794
gmn14 <meza.gustavo@pucp.edu.pe>
2020-05-07 23:31:02 -0500

    3

--- src/main/java/com/example/sw2/utils/Credential.java
+++ src/main/java/com/example/sw2/utils/Credential.java
@@ -4,6 +4,5 @@ final class Credential {
     final static String REGION_ID = "";
     final static String ACCESS_KEY_ID = "";
     final static String SECRET_KEY_ID = "";
-    final static String SESSION_TOKEN = "";
 
 }
--- src/main/java/com/example/sw2/utils/UploadObject.java
+++ src/main/java/com/example/sw2/utils/UploadObject.java
@@ -4,6 +4,7 @@ import com.amazonaws.AmazonServiceException;
 import com.amazonaws.SdkClientException;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.InstanceProfileCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
@@ -20,15 +21,12 @@ public class UploadObject {
         String bucketName = "test-bucket-sw2";
         String stringObjKeyName = "test_object";
         String fileObjKeyName = "test_key";
-        String fileName = "/home/ubuntu/test.txt ";
-
+        //String fileName = "/home/ubuntu/test.txt";
+        String fileName = "/Users/Gustavo_Meza/Desktop/aaa/test.txt";
         try {
             //This code expects that you have AWS credentials set up per:
             // https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/setup-credentials.html
-            BasicAWSCredentials awsCreds =
-                    new BasicAWSCredentials(Credential.ACCESS_KEY_ID, Credential.SECRET_KEY_ID);
             AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
-                    .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
                     .withRegion(clientRegion)
                     .build();
 
@@ -42,6 +40,7 @@ public class UploadObject {
             metadata.addUserMetadata("title", "someTitle");
             request.setMetadata(metadata);
             s3Client.putObject(request);
+
         } catch (AmazonServiceException e) {
             // The call was transmitted successfully, but Amazon S3 couldn't process
             // it, so it returned an error response.
