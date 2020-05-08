294bf8200c7f8d55ad8f4b3bcc94eb6b0d488370
gmn14 <meza.gustavo@pucp.edu.pe>
2020-05-08 16:25:09 -0500

    test

--- src/main/java/com/example/sw2/utils/CustomMailService.java
+++ src/main/java/com/example/sw2/utils/CustomMailService.java
@@ -2,16 +2,10 @@ package com.example.sw2.utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.io.File;
