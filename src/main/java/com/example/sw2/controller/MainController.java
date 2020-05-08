d05204abd6960f370fe23d14e89ec2737b03f21c
gmn14 <meza.gustavo@pucp.edu.pe>
2020-05-08 14:00:41 -0500

    email

--- src/main/java/com/example/sw2/controller/MainController.java
+++ src/main/java/com/example/sw2/controller/MainController.java
@@ -1,19 +1,26 @@
 package com.example.sw2.controller;
 
+import com.example.sw2.utils.CustomMailService;
 import com.example.sw2.utils.DeleteObjectNonVersionedBucket;
 import com.example.sw2.utils.UploadObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.mail.MessagingException;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    CustomMailService customMailService;
 
     private final String FILE_NAME = "test.png";
 
@@ -48,6 +55,19 @@ public class MainController {
             System.out.println("error");
         }
         return "redirect:/file";
+    }
+
+    @ResponseBody
+    @GetMapping(value = "/testEmail",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> testEmail(){
+
+        try {
+            customMailService.sendTestEmailWithAtachment();
+            return new ResponseEntity<>("nice", HttpStatus.OK);
+        }
+        catch (IOException | MessagingException ex){
+            return new ResponseEntity<>(ex.toString(), HttpStatus.ACCEPTED);
+        }
     }
 
 }
