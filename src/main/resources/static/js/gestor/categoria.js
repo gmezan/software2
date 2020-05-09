14cd3d6f8de1b62d33075621224bb51b53561f97
gmn14 <meza.gustavo@pucp.edu.pe>
2020-05-09 15:19:09 -0500

    js

--- src/main/resources/static/js/gestor/categoria.js
+++ src/main/resources/static/js/gestor/categoria.js
@@ -1,30 +1,30 @@
 var contextPath = "http://localhost:8080";
 
 $(document).on("click",".editar-Categoria", function(){
-    $("#editModal  #codigo").val(' ');$("#editModal  #nombre").val('');
+    $("#formModal  #codigo").val(' ');$("#formModal  #nombre").val('');
     $.ajax({
         method:"GET", url:contextPath + "/gestor/categoria/get?id=" + $(this).data('id')
     }).done(function(cat){
         if (cat!=null){
-            $("#editModal  #codigo").val(cat.codigo).prop("readonly", true);
-            $("#editModal #nombre").val(cat.nombre);
+            $("#formModal  #codigo").val(cat.codigo).prop("readonly", true);
+            $("#formModal #nombre").val(cat.nombre);
         }
     }).fail(function (err) {
         console.log(err);
-        $('#editModal').modal('hide');
+        $('#formModal').modal('hide');
         alert("Ocurrió un error");
     })
 });
 $(document).on("click",".new-Categoria", function(){
-    $("#editModal  #codigo").val('');
-    $("#editModal  #nombre").val('');
+    $("#formModal  #codigo").val('');
+    $("#formModal  #nombre").val('');
 });
 $(document).on("click",".delete-Categoria", function(){
     $("#deleteModal #codigo").val($(this).data('id'));
 });
 $(document).ready(function() {
     if ($("#msgCategoria").text()==="ERROR"){
-        $('#editModal').modal('show');
+        $('#formModal').modal('show');
     }
 });
 
