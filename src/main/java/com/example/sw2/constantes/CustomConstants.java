3a27aa78e0bbac6edbe01d422f650b0038eaf231
AlonsoGuevara15 <a20160679@pucp.edu.pe>
2020-05-15 06:05:22 -0500

    avanceinventario +constantes

--- src/main/java/com/example/sw2/constantes/CustomConstants.java
+++ src/main/java/com/example/sw2/constantes/CustomConstants.java
@@ -1,17 +1,55 @@
 package com.example.sw2.constantes;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 
 public final class CustomConstants {
 
     private static final HashMap<Integer, String> lineas = new HashMap<>();
-    private static final HashMap<Integer,String> MESES = new HashMap<>();
+    private static final HashMap<Integer, List<String>> meses = new HashMap<>();
+    private static final HashMap<Integer, String> tiposAdquisicion = new HashMap<>();
+    private static final HashMap<Integer, String> tiposDocumento = new HashMap<>();
+    private static final LinkedHashMap<String, String> tamanhos = new LinkedHashMap<>();
+
     static {
         lineas.put(1, "Tradicional");
         lineas.put(2, "Mosqoy");
         lineas.put(3, "Fibras");
 
-        MESES.put(1,"JAN");
+
+        List<String> jan = new ArrayList<>();
+        jan.add("JAN");
+        jan.add("Enero");
+        List<String> feb = new ArrayList<>();
+        feb.add("FEB");
+        feb.add("Febrero");
+        List<String> mar = new ArrayList<>();
+        mar.add("MAR");
+        mar.add("Marzo");
+        List<String> apr = new ArrayList<>();
+        apr.add("APR");
+        apr.add("Abril");
+        List<String> may= new ArrayList<>();
+        may.add("MAY");
+        may.add("Mayo");
+
+        meses.put(1,jan);
+        meses.put(2,feb);
+        meses.put(3,mar);
+        meses.put(4,apr);
+        meses.put(5,may);
+
+        tamanhos.put("OS","One-Size");
+        tamanhos.put("S","Small");
+        tamanhos.put("M","Medium");
+        tamanhos.put("L","Large");
+
+        tiposAdquisicion.put(0,"Comprado");
+        tiposAdquisicion.put(1,"Consignado");
+
+
     }
 
 
@@ -19,4 +57,15 @@ public final class CustomConstants {
         return lineas;
     }
 
+    public static HashMap<Integer, List<String>> getMeses() {
+        return meses;
+    }
+
+    public static LinkedHashMap<String, String> getTamanhos() {
+        return tamanhos;
+    }
+
+    public static HashMap<Integer, String> getTiposAdquisicion() {
+        return tiposAdquisicion;
+    }
 }
--- src/main/java/com/example/sw2/controller/gestor/InventarioController.java
+++ src/main/java/com/example/sw2/controller/gestor/InventarioController.java
@@ -1,5 +1,6 @@
 package com.example.sw2.controller.gestor;
 
+import com.example.sw2.constantes.CustomConstants;
 import com.example.sw2.entity.Categorias;
 import com.example.sw2.entity.Inventario;
 import com.example.sw2.repository.CategoriasRepository;
@@ -16,13 +17,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class InventarioController {
     @Autowired
     InventarioRepository inventarioRepository;
-
+@Autowired
+CategoriasRepository categoriasRepository;
 
     @GetMapping(value = {"", "/"})
-    public String listInv(@ModelAttribute("inventario") Inventario inventario, Model model) {
-        model.addAttribute("listaInv", inventarioRepository.findAll());
+    public String listInv(@ModelAttribute("inventario") Inventario inventario, Model m) {
+        m.addAttribute("listaInv", inventarioRepository.findAll());
+        m.addAttribute("Meses", CustomConstants.getMeses());
+        m.addAttribute("taman", CustomConstants.getTamanhos());
         return "gestor/inventarioGestor";
     }
 
+    @GetMapping(value = {"/form"})
+    public String form(@ModelAttribute("inventario") Inventario inventario, Model m) {
 
+        m.addAttribute("Meses", CustomConstants.getMeses());
+        m.addAttribute("taman", CustomConstants.getTamanhos());
+        m.addAttribute("tipoAdqui", CustomConstants.getTiposAdquisicion());
+        m.addAttribute("lineas", CustomConstants.getLineas());
+        m.addAttribute("listCat", categoriasRepository.findAll());
+        return "gestor/inventarioGestorForm";
+    }
 }
