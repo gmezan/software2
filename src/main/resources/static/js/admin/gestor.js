df0ebf06e42e86e14ff7542ec542ce7fb9599ec4
root <root@ip-172-31-92-136.ec2.internal>
2020-05-17 21:11:21 +0000

    gestor.js

---
+++ src/main/resources/static/js/admin/gestor.js
@@ -0,0 +1,47 @@
+var contextPath  = window.location.href+"/get?id=";
+
+$(document).on("click",".edit-Gestor", function(){
+    $("#formModal #idusuarios").val('');
+    $("#formModal #nombre").val('');
+    $("#formModal #apellido").val('');
+    $("#formModal #correo").val('');
+    $("#formModal #telefono").val('');
+    $("#formModal #password").val('');
+    $.ajax({
+        method:"GET", url:contextPath + $(this).data('id')
+    }).done(function(gestor){
+        if (gestor!=null){
+            $("#formModal #idusuarios").val(gestor.idusuarios).prop("readonly", true);
+            $("#formModal #nombre").val(gestor.nombre);
+            $("#formModal #apellido").val(gestor.apellido);
+            $("#formModal #correo").val(gestor.correo);
+            $("#formModal #telefono").val(gestor.telefono);
+            $("#formModal #password").val(gestor.password);
+            $("#formModal #foto").attr("src",gestor.foto);
+            if (gestor.foto===""){
+                $("#formModal #foto").attr("hidden",true);
+            }
+
+        }
+    }).fail(function (err) {
+        console.log(err);
+        $('#formModal').modal('hide');
+        alert("Ocurrió un error");
+    })
+});
+$(document).on("click",".new-Gestor", function(){
+    $("#formModal #idusuarios").val('');
+    $("#formModal #nombre").val('');
+    $("#formModal #apellido").val('');
+    $("#formModal #correo").val('');
+    $("#formModal #telefono").val('');
+    $("#formModal #password").val('');
+});
+$(document).on("click",".delete-Gestor", function(){
+    $("#deleteModal #idusuarios").val($(this).data('id'));
+});
+$(document).ready(function() {
+    if ($("#msgGestores").text()==="ERROR"){
+        $('#formModal ').modal('show');
+    }
+});
