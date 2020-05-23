d4de776e4558992ebf1dd5161fc4b95c6d54e844
Alejandro Noblecilla <ale_14_nb@hotmail.com>
2020-05-23 04:43:51 -0500

    ...

--- src/main/java/com/example/sw2/repository/AsignadosSedesRepository.java
+++ src/main/java/com/example/sw2/repository/AsignadosSedesRepository.java
@@ -24,11 +24,11 @@ public interface AsignadosSedesRepository extends JpaRepository<AsignadosSedes, 
     @Query(value="select p.nombre as nombreproducto," +
             "a.cantidadactual as cantidad, u.nombre as nombre, u.foto as foto, u.apellido as apellido,"+
             "u.correo as correo, u.telefono as telefono\n"+
-            "FROM Asignados_sedes a\n"+
+            "FROM Asignados_sedes a where a.estadoasignacion = 4\n"+
             "inner join Inventario i on (a.producto_inventario = i.codigo_inventario)\n" +
             "inner join Productos p on (i.producto = p.codigonom)\n"+
             "inner join Usuarios u on (a.gestor = u.dni)\n"+
-            "group by a.estadoasignacion = 4",
+            "group by a.estadoasignacion",
             nativeQuery = true)
     List<DevolucionDto> DatosDevolucion();
 
