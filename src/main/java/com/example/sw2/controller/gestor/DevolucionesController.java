2661c51a2c3f83d3c9d12b49c2e3b92bb04e1356
Alejandro Noblecilla <ale_14_nb@hotmail.com>
2020-05-23 03:35:28 -0500

    devoluciones

---
+++ src/main/java/com/example/sw2/controller/gestor/DevolucionesController.java
@@ -0,0 +1,32 @@
+package com.example.sw2.controller.gestor;
+
+import com.example.sw2.entity.AsignacionTiendas;
+import com.example.sw2.entity.AsignadosSedes;
+import com.example.sw2.entity.Ventas;
+import com.example.sw2.repository.AsignadosSedesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/devoluciones")
+public class DevolucionesController {
+
+    @Autowired
+    AsignadosSedesRepository asignadosSedesRepository;
+
+
+    @GetMapping(value = {"", "/"})
+    public String ListaDevoluciones(Model model){
+
+        model.addAttribute("devueltos", asignadosSedesRepository.findAll());
+        return "/gestor/devoluciones";
+    }
+
+    @PostMapping("/confirmar")
+    public String Confirmar(){
+        return "redirect:/devoluciones/";
+    }
+}
