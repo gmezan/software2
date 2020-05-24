690776ef3face4553e712a8bac45f14154deadc2
AlonsoGuevara15 <a20160679@pucp.edu.pe>
2020-05-23 20:03:47 -0500

    estadoasignacion borrado

--- src/main/java/com/example/sw2/repository/EstadoAsignacionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.sw2.repository;
-
-import com.example.sw2.entity.EstadoAsignacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EstadoAsignacionRepository extends JpaRepository<EstadoAsignacion,Integer> {
-}
