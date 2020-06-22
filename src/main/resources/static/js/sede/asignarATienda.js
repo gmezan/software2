be751218216fde266e01f8b92287ebf076ec1f25
allisonvp <a.velardep@pucp.edu.pe>
2020-06-22 04:23:06 -0500

    sede

--- src/main/resources/static/js/sede/asignarATienda.js
+++ src/main/resources/static/js/sede/asignarATienda.js
@@ -3,11 +3,11 @@ var contextPath = window.location.href;
 $(document).on( "click", ".asign-A-Tienda", function () {
 
 
-    $("#AsignarProductoModal  #idgestor2").val($(this).data('id1'));
-    $("#AsignarProductoModal  #idsede2").val($(this).data('id2'));
-    $("#AsignarProductoModal  #idproductoinventario2").val($(this).data('id3'));
-    $("#AsignarProductoModal  #idestadoasign2").val($(this).data('id4'));
-    $("#AsignarProductoModal  #idprecioventa2").val($(this).data('id5'));
+    $("#AsignarProductoModal  #asignadosSedes\\.id\\.gestor").val($(this).data('id1'));
+    $("#AsignarProductoModal  #asignadosSedes\\.id\\.sede").val($(this).data('id2'));
+    $("#AsignarProductoModal  #asignadosSedes\\.id\\.productoinventario").val($(this).data('id3'));
+    $("#AsignarProductoModal  #asignadosSedes\\.id\\.estadoasignacion").val($(this).data('id4'));
+    $("#AsignarProductoModal  #asignadosSedes\\.id\\.precioventa").val($(this).data('id5'));
 
     $("#AsignarProductoModal  #fechaasignacion").val('');
     $("#AsignarProductoModal  #stock").val('');
