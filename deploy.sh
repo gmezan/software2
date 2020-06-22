6cbc9120fba6bdbe6182f0e6ae6e4989cabd3310
Ubuntu <ubuntu@ip-172-31-61-109.ec2.internal>
2020-06-22 05:45:32 +0000

    udpate sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,20 @@
 echo "Warning running: sudo rm -r /var/sw2/sw2.jar"
 echo "For deployment information, go to: https://docs.spring.io/spring-boot/docs/current/reference/html/deployment.html#deployment-systemd-service"
+sudo systemctl stop sw2.service
 sudo rm -r /var/sw2/sw2.jar
 sudo git pull
 sudo mvn package
 sudo cp target/sw2-0.0.1-SNAPSHOT.jar /var/sw2/sw2.jar
+sudo systemctl enable sw2.service
+sudo systemctl start sw2.service
 echo "Script finished"
+sudo systemctl status sw2.service
+echo "Wait...1"
+sleep 6
+sudo systemctl status sw2.service
+echo "Wait...2"
+sleep 6
+sudo systemctl status sw2.service
+echo "Wait...3"
+sleep 6
+sudo systemctl status sw2.service
