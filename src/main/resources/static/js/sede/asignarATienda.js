c742dfa40fd478af91b190a78fbc437fa1213fff
gmn14 <meza.gustavo@pucp.edu.pe>
2020-06-23 15:04:08 -0500

    debug sede 4

--- src/main/resources/static/js/sede/asignarATienda.js
+++ src/main/resources/static/js/sede/asignarATienda.js
@@ -5,7 +5,7 @@ $(document).on( "click", ".asign-A-Tienda", function () {
 
     $("#AsignarProductoModal  #asignadosSedes\\.id\\.gestor").val($(this).data('id1'));
     $("#AsignarProductoModal  #asignadosSedes\\.id\\.sede").val($(this).data('id2'));
-    $("#AsignarProductoModal  #asignadosSedes\\.id\\.productoinventario").val($(this).data('id3'));
+    $("#AsignarProductoModal  #asignadosSedes\\.id\\.productoinventario\\.codigoinventario").val($(this).data('id3'));
     $("#AsignarProductoModal  #asignadosSedes\\.id\\.estadoasignacion").val($(this).data('id4'));
     $("#AsignarProductoModal  #asignadosSedes\\.id\\.precioventa").val($(this).data('id5'));
 
