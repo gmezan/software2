b0c6f5eace4d8d2063f96395bda281a76a660285
AlonsoGuevara15 <a20160679@pucp.edu.pe>
2020-06-22 19:06:56 -0500

    mr CRUD

--- src/main/java/com/example/sw2/repository/InventarioRepository.java
+++ src/main/java/com/example/sw2/repository/InventarioRepository.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 @Repository
 public interface InventarioRepository extends JpaRepository<Inventario,String> {
     List<Inventario> findAllByOrderByFechamodificacionDesc();
+    List<Inventario> findAllByOrderByNumpedidoDesc();
     List<Inventario> findInventariosByArtesanos_Codigo(String codigo);
     Inventario findByCodigoinventario(String cod);
     Optional<Inventario> findInventariosByNumpedido(int num);
