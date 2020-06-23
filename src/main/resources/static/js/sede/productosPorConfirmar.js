3c7eca1b565cdf2b2c0941b8aeb48b12bc2167c4
gmn14 <meza.gustavo@pucp.edu.pe>
2020-06-23 16:18:58 -0500

    debug sede 7

--- src/main/resources/static/js/sede/productosPorConfirmar.js
+++ src/main/resources/static/js/sede/productosPorConfirmar.js
@@ -23,23 +23,19 @@ $(document).on("click",".conf-Produc", function(){
         //+ $(this).data('id1') + "&idsede=" + $(this).data('id2')
         //+ "&idproductoinv=" + $(this).data('id3') + "&idfechaenvio=" + $(this).data('id4');
 
-    var data = {
+    let data = {
         gestor: parseInt($(this).data('id1')),
         sede: parseInt($(this).data('id2')),
         productoinventario: $(this).data('id3'),
-        estadoasignacion: $(this).data('id4'),
-        precioventa: $(this).data('id5')};
+        estadoasignacion: parseInt($(this).data('id4')),
+        precioventa: parseFloat($(this).data('id5'))
+    };
 
     console.log(data);
     $.ajax({
-        async: false,
-        dataType : "json",
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        method:"POST", url:url,
-        data: data
+        method:"POST",
+        data: data,
+        url:url
     }).done(function(asignsede){
         if (asignsede!=null){
             console.log(asignsede);
