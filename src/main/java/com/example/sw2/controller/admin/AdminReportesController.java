12c6baae5452f3f42373550fc058554ed65dbd9b
Alex Montes <montes.alex@pucp.edu.pe>
2020-06-23 11:21:20 -0500

    asdmo12sda

--- src/main/java/com/example/sw2/controller/admin/AdminReportesController.java
+++ src/main/java/com/example/sw2/controller/admin/AdminReportesController.java
@@ -1,16 +1,17 @@
-package com.example.sw2.controller.gestor;
+package com.example.sw2.controller.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/gestor/reportes")
-public class ReportesGestorController {
+@RequestMapping("/admin/reportes")
+public class AdminReportesController {
 
     @GetMapping(value = "")
     public String listaReportes(){
-        return "gestor/Reportes";
-//        return "gestor/en_construccion";
+
+//        return "reportes";
+          return "admin/reportesEnConstruccion";
     }
 }
