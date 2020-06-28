94ec0ee69e0b420d2712c4f0c2c06a9ec54613ed
Alex Montes <montes.alex@pucp.edu.pe>
2020-06-28 01:09:42 -0500

    asdimdasi123damki

--- src/main/java/com/example/sw2/controller/gestor/ReportesGestorController.java
+++ src/main/java/com/example/sw2/controller/gestor/ReportesGestorController.java
@@ -1,5 +1,6 @@
 package com.example.sw2.controller.gestor;
 
+import com.example.sw2.entity.Reportes;
 import com.example.sw2.service.ServiceReportes;
 import com.example.sw2.service.ServiceReportes2222;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,9 +27,6 @@ public class ReportesGestorController {
     @Autowired
     ServiceReportes2222 serviceReportes2222;
 
-    //////inicio prueba excel
-    @Autowired
-    ServiceVentaAnualXxProducto serviceVentaAnualXxProducto;
 
     @GetMapping(value = "")
     public String listaReportes(){
@@ -35,40 +34,16 @@ public class ReportesGestorController {
 //        return "gestor/en_construccion";
     }
 
-    @GetMapping(value = {"/excelAnualXxProducto"})
-    public ResponseEntity<InputStreamResource> excelAnualXxProducto() throws Exception{
 
-        ByteArrayInputStream stream = serviceVentaAnualXxProducto.exportAllData();
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Disposition","attachment; filename=ventasProducto.xls");
-
-        return ResponseEntity.ok().headers(headers).body(new InputStreamResource(stream));
-    }
     //fin rpueba excel
 
 
 
     @PostMapping(value = "/save")
-    public ResponseEntity<InputStreamResource> printExcel(@RequestParam("ordenar") int orderBy, @RequestParam("tipo") int type,
-                                                          @RequestParam("years") int anho, @RequestParam("tipoSelect") int Select)
+    public ResponseEntity<InputStreamResource> printExcel(Reportes reportes)
                                                             throws Exception{
 
-        int trimester = 0;
-        int mes = 0;
-        if (type == 1){
-            trimester = 0;
-            mes = 0;
-        }else if (type == 2){
-            trimester = Select;
-            mes = 0;
-        }else if(type == 3){
-            trimester = 0;
-            mes = Select;
-        }
-
-        System.out.println("El orderBy es: " + orderBy);
-        ByteArrayInputStream stream = serviceReportes2222.SedeOrClienteXxAnual_TrimesterOrMonth(mes, trimester, anho, orderBy, type);
+        ByteArrayInputStream stream = serviceReportes2222.generarReporte(reportes);
 
         HttpHeaders headers = new HttpHeaders();
         headers.add("Content-Disposition","attachment; filename=ventasProducto.xls");
--- src/main/java/com/example/sw2/controller/gestor/VentasGestorController.java
+++ src/main/java/com/example/sw2/controller/gestor/VentasGestorController.java
@@ -27,8 +27,6 @@ import java.util.regex.Pattern;
 @RequestMapping("/gestor/venta")
 public class VentasGestorController {
 
-    @Autowired
-    ServiceAnual serviceAnual;
 
     @Autowired
     VentasRepository ventasRepository;
@@ -142,7 +140,7 @@ public class VentasGestorController {
 
     //EXCEL
 
-    @GetMapping(value = {"/excelAnual2020"})
+/*    @GetMapping(value = {"/excelAnual2020"})
     public ResponseEntity<InputStreamResource> exportAllData() throws Exception{
 
         ByteArrayInputStream stream = serviceAnual.exportAllData();
@@ -151,7 +149,7 @@ public class VentasGestorController {
         headers.add("Content-Disposition","attachment; filename=ventasAnual.xls");
 
         return ResponseEntity.ok().headers(headers).body(new InputStreamResource(stream));
-    }
+    }*/
 
 
 
