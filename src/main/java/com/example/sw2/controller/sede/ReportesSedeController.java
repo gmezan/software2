0686daedee21802d4b7d3ec399a73f52394e197f
gmn14 <meza.gustavo@pucp.edu.pe>
2020-07-18 11:13:58 -0500

    getYears para Reportes en admin & sede

--- src/main/java/com/example/sw2/controller/sede/ReportesSedeController.java
+++ src/main/java/com/example/sw2/controller/sede/ReportesSedeController.java
@@ -2,19 +2,19 @@ package com.example.sw2.controller.sede;
 
 import com.example.sw2.entity.Reportes;
 import com.example.sw2.entity.Usuarios;
+import com.example.sw2.repository.VentasRepository;
 import com.example.sw2.service.IReporteSedeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/sede/reportes")
@@ -23,6 +23,8 @@ public class ReportesSedeController {
 
     @Autowired
     IReporteSedeService IReporteSedeService;
+    @Autowired
+    VentasRepository ventasRepository;
 
     @Autowired
 
@@ -55,5 +57,14 @@ public class ReportesSedeController {
     }
 
 
+    @ResponseBody
+    @GetMapping(value = "/years")
+    public ResponseEntity<ArrayList<Integer>> getYears(){
+        return new ResponseEntity<>(new ArrayList<Integer>(){
+            {
+                this.addAll(ventasRepository.getYears());
+            }
+        }, HttpStatus.OK);
+    }
 
 }
